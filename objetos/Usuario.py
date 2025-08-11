954f7be5a18bdca580cbc47c34db2aa27935beb5
NelCR1229 <david1rocha311@gmail.com>
2025-08-10 21:16:12 -0600

    Actualiza Usuario.py

--- objetos/Usuario.py
+++ objetos/Usuario.py
@@ -1,1 +1,9 @@
 # Representa al usuario que realiza las ventas
+
+class Usuario:
+    def __init__(self, id, nombre):
+        self.id = id
+        self.nombre = nombre
+
+    def __str__(self):
+        return f"Comprado por {self.nombre} con el id {self.id}"
