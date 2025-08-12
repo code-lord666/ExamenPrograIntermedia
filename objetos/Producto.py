9cb8a2be6e5dc935e76074b50c9fc618e805510a
code-lord666 <gxchaves@gmail.com>
2025-08-11 23:42:08 -0600

    w

--- objetos/Producto.py
+++ objetos/Producto.py
@@ -4,7 +4,7 @@ class Producto:
         self.id_producto =+ 100
         self.nombre_Producto = nombre_Producto
         self.precio = precio
-        self.cantidad = cantidad #
+        self.cantidad = cantidad 
     
     def __str__(self):
         return f"Producto :{self.nombre_Producto} Id :{self.id_producto}"
